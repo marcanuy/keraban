1a413c61cc1ab1d9b34aca0e2ef6101ff35b7b58
Marcelo Canina <me@marcanuy.com>
2018-05-22 15:24:16 -0300

    fix #3 add breadcrumb with schema

--- home/templatetags/navigation_tags.py
+++ home/templatetags/navigation_tags.py
@@ -71,3 +71,19 @@ def top_menu_children(context, parent, calling_page=None):
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
     }
+
+
+@register.inclusion_tag('tags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self')
+    if self is None or self.depth <= 2:
+        # When on the home page, displaying breadcrumbs is irrelevant.
+        ancestors = ()
+    else:
+        ancestors = Page.objects.ancestor_of(
+            self, inclusive=True).filter(depth__gt=1)
+    return {
+        'ancestors': ancestors,
+        'request': context['request'],
+    }
+
