2456b822494bf403b88a569e1bffe9829e1e797d
Marcelo Canina <me@marcanuy.com>
2018-05-21 23:34:17 -0300

    add location homepage migration and tag

---
+++ home/templatetags/location_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from django.conf import settings
+
+from wagtail.images.models import Image
+
+from home.models import Businesses
+
+register = template.Library()
+
+@register.inclusion_tag('tags/map.html', takes_context=True)
+def show_map(context):
+    business = Businesses.objects.first()
+    pos = business.geo_coordinates.split(',')
+
+    return {
+        'address' : business.address,
+        'lat' : pos[0],
+        'long' : pos[1],
+        'google_map_api_key' : settings.GOOGLE_MAP_API_KEY,
+        'request': context['request'],
+    }
