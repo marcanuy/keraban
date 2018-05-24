640a7648f3eb1234399bb10107d4e9a81e4ce739
Marcelo Canina <me@marcanuy.com>
2018-05-24 16:20:40 -0300

    load dotenv if .env file exists

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@
 import os
 import sys
 
+import dotenv
+
+
 if __name__ == "__main__":
+    env_file = '.env'
+    if os.path.isfile(env_file):
+        dotenv.read_dotenv(env_file)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keraban.settings.dev")
 
     from django.core.management import execute_from_command_line
