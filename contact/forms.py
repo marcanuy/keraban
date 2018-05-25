3c3ca8e4109f38afdda440fa7acfc42af0170c58
Marcelo Canina <me@marcanuy.com>
2018-05-25 10:30:58 -0300

    adding translation keys to templates

--- contact/forms.py
+++ contact/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length=100)
-    email = forms.CharField(label='Your email', max_length=100)
-    phone = forms.CharField(label="Business email address",
+    name = forms.CharField(label=_('Your name'), max_length=100)
+    email = forms.CharField(label=_('Your email'), max_length=100)
+    phone = forms.CharField(label=_("Phone"),
                              max_length=100,
                              required=False)
 
-    message = forms.CharField(label='Message', max_length=500)
+    message = forms.CharField(label=_('Message'), max_length=500, widget=forms.Textarea)
