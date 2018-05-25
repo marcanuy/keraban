625d4942f47b6fa03754ef381b2e8c8be810c5c7
Marcelo Canina <me@marcanuy.com>
2018-05-25 11:09:18 -0300

    add bootstrap 4 styling to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label=_('Your name'), max_length=100)
-    email = forms.CharField(label=_('Your email'), max_length=100)
+    name = forms.CharField(label=_('Your name'),
+                           max_length=100,
+                           widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.CharField(label=_('Your email'),
+                            max_length=100,
+                            widget=forms.EmailInput(attrs={'class': 'form-control'}))
     phone = forms.CharField(label=_("Phone"),
-                             max_length=100,
-                             required=False)
-
-    message = forms.CharField(label=_('Message'), max_length=500, widget=forms.Textarea)
+                            max_length=100,
+                            required=False,
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(label=_('Message'),
+                              max_length=500,
+                              widget=forms.Textarea(attrs={'class': 'form-control'}))
